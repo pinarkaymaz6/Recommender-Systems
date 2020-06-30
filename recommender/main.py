c43b68ff9e9d258fdb2dd3a1d76748f7033145c5
Pinar Kaymaz <pinar.kaymaz@tum.de>
2020-06-30 10:00:39 +0200

    Add movies page

--- recommender/main.py
+++ recommender/main.py
@@ -1,28 +1,19 @@
-import datetime
-
+import pandas as pd
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+moviemat = pd.read_pickle("movie_matrix.pkl")
 
 @app.route('/')
 def root():
-    # For the sake of example, use static information to inflate the template.
-    # This will be replaced with real information in later steps.
-    dummy_times = [datetime.datetime(2018, 1, 1, 10, 0, 0),
-                   datetime.datetime(2018, 1, 2, 10, 30, 0),
-                   datetime.datetime(2018, 1, 3, 11, 0, 0),
-                   ]
+    return render_template('index.html')
 
-    return render_template('index.html', times=dummy_times)
 
+@app.route('/movies')
+def movies():
+    movie_list = list(moviemat.columns[:5])
+    return render_template('movies.html', mlist=movie_list)
 
 if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
-    # Flask's development server will automatically serve static files in
-    # the "static" directory. See:
-    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
-    # App Engine itself will serve those files as configured in app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
